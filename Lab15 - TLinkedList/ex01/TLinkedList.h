a3f0f4932ea8875c3fc2100e0984b6ba8d38947a
lucasrcosta1ufu <80076371+lucasrcosta1ufu@users.noreply.github.com>
2021-05-09 10:08:37 -0300

    Update TLinkedList.h

--- Lab15 - TLinkedList/ex01/TLinkedList.h
+++ Lab15 - TLinkedList/ex01/TLinkedList.h
@@ -3,6 +3,7 @@
 #define OUT_OF_MEMORY -2
 #define OUT_OF_RANGE -3
 #define ELEM_NOT_FOUND -4
+#define INVALID_POSITION -5
 
 struct aluno{
     int matricula;
@@ -12,25 +13,28 @@ struct aluno{
 
 typedef struct TLinkedList TLinkedList;
 
-TLinkedList *list_create();
-int list_free(TLinkedList *list);
+TLinkedList *list_create(); //Done
+int list_free(TLinkedList *list); //Done
 
-int list_push_front(TLinkedList *list, struct aluno al);
-int list_push_back(TLinkedList *list, struct aluno al);
+int list_push_front(TLinkedList *list, struct aluno al); //Done
+int list_push_back(TLinkedList *list, struct aluno al); //Done
 
-int list_insert(TLinkedList *list, int pos, struct aluno al);
-int list_insert_sorted(TLinkedList *list, struct aluno al);
-int list_size(TLinkedList *list);
+int list_insert(TLinkedList *list, int pos, struct aluno al); //Done
+int list_insert_sorted(TLinkedList *list, struct aluno al); //Done
+int list_size(TLinkedList *list); //Done
 
 
-int list_pop_front(TLinkedList *list);
-int list_pop_back(TLinkedList *list);
-int list_erase(TLinkedList *list, int pos);
+int list_pop_front(TLinkedList *list); //Done
+int list_pop_back(TLinkedList *list); //Done
+int list_erase_data(TLinkedList *list, int mat); //Done
+int list_erase_pos(TLinkedList *list, int pos); //Not Done
 
-int list_find_pos(TLinkedList *list, int pos, struct aluno *al);
+int list_find_pos(TLinkedList *list, int pos, struct aluno *al); //Done
 int list_find_mat(TLinkedList *list, int nmat, struct aluno *al);
+int list_front(TLinkedList *list, struct aluno **al); //Done
+int list_back(TLinkedList *list, struct aluno **al); //Done
 int list_front(TLinkedList *list, struct aluno *al);
-
-int list_back(TLinkedList *list, struct aluno *al);
 int list_get_pos(TLinkedList *list, int nmat, int *pos);
-int list_print(TLinkedList *list);
+int list_print(TLinkedList *list); //Done
+void node_print(struct aluno *al); //Done
+void print_menu();
